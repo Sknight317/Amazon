286bb6748bf90835c0c07aa7dbbc9f8b88378732
Sknight317 <Sknight23@mail.valenciacollege.edu>
2018-11-03 18:36:08 -0400

    added to js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,6 +4,9 @@ var mysql = require("mysql");
 // require inquirer
 var inquirer = require("inquirer");
 
+// used to require console.table
+const cTable = require('console.table');
+
 // create the connection information for the sql database
 var connection = mysql.createConnection({
     host: "localhost",
@@ -25,7 +28,31 @@ var connection = mysql.createConnection({
   function afterConnection() {
     connection.query("SELECT * FROM products", function(err, data) {
       if (err) throw err;
-      console.log(data);
-      connection.end();
+      console.table(data);
+      start();
+    });
+  }
+
+  function start() {
+    inquirer
+    .prompt([
+      {
+      name: "id",
+      type: "input",
+      message: "What is the ID of the item you would like to purchase?",
+      
+    },
+    {
+      name: "Quantity",
+      type: "input",
+      message: "How many would you like?",
+      
+    },
+  ])
+    .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.pick === "Find songs by artist") {
+        findArtist();
+      }
     });
   }
